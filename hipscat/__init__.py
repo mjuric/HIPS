1c610de5c7256b6b3a8c126ff4821c725dad2038
Mario Juric <mjuric@astro.washington.edu>
2021-11-14 14:13:08 -0800

    Add HDF5 support to the distributed loader

--- hipscat/__init__.py
+++ hipscat/__init__.py
@@ -14,7 +14,7 @@ def pix2subdir(k, pix):
 
 ########################
 
-def _to_hips(df, hipsPath, outFn='catalog.csv'):
+def _to_hips(df, hipsPath, outFn='catalog', format='csv'):
     # WARNING: only to be used from df2hips(); it's defined out here just for debugging
     # convenience.
 
@@ -27,19 +27,24 @@ def _to_hips(df, hipsPath, outFn='catalog.csv'):
 
     # construct the output directory and filename
     dir = os.path.join(hipsPath, pix2subdir(k, pix))
-    fn  = os.path.join(dir, outFn)
+    fn  = os.path.join(dir, f"{outFn}.{format}")
 
     # create dir if it doesn't exist
     os.makedirs(dir, exist_ok=True)
 
-    # write to the file (append if it already exists)
-    # also, write the header only if the file doesn't already exist
-    df.to_csv(fn, mode='a', index=False, header=not os.path.exists(fn))
+    if format == 'csv':
+        # write to the file (append if it already exists)
+        # also, write the header only if the file doesn't already exist
+        df.to_csv(fn, mode='a', index=False, header=not os.path.exists(fn))
+    elif format == 'pkl':
+        # note: storing to pickle doesn't support appending
+        assert not os.path.exists(fn)
+        df.to_pickle(fn)
 
     # return the number of records written
     return len(df)
 
-def df2hips(hipsPath, df, k, outFn='catalog.csv'):
+def df2hips(hipsPath, df, k, outFn='catalog.csv', format='csv'):
     # Dataframe-to-hips: write out a dataframe as a HiPS file of a given order
     # FIXME: rewrite this implementation so it doesn't modify the input dataframe
 
@@ -48,7 +53,7 @@ def df2hips(hipsPath, df, k, outFn='catalog.csv'):
     df['hips_pix'] = hp.ang2pix(2**k, df['ra'].values, df['dec'].values, lonlat=True)
 
     # returns the number of records written to each HiPS file
-    return df.groupby(['hips_k', 'hips_pix']).apply(_to_hips, hipsPath=hipsPath, outFn=outFn)
+    return df.groupby(['hips_k', 'hips_pix']).apply(_to_hips, hipsPath=hipsPath, outFn=outFn, format=format)
 
 
 ########################
@@ -63,23 +68,29 @@ def _partition_worker(url, hipsPath, k):
 
     # write the file into 'catalog-Gaia...csv.gz' named files
     outFn = 'import.' + os.path.basename(url)
-    ret = df2hips(hipsPath, df, k=k, outFn=outFn)
+    ret = df2hips(hipsPath, df, k=k, outFn=outFn, format='pkl')
     return ret
 
-def _merge_worker(idx, hipsPath, outFn):
+def _merge_worker(idx, hipsPath, outFn, format):
     # for a given idx=(k, pix), list all 'import.*' files and merge them
     # into outFn (usually catalog.csv). Delete the import.* files once
     # the merge is done.
     k, pix = idx
     dir = os.path.join(hipsPath, pix2subdir(k, pix))
-    destFn = os.path.join(dir, outFn)
+    destFn = os.path.join(dir, f'{outFn}.{format}')
 
-    for fn in glob.glob(os.path.join(dir, 'import.*')):
-        df = pd.read_csv(fn)
-        df.to_csv(destFn, mode='a', index=False, header=not os.path.exists(destFn))
+    for fn in glob.glob(os.path.join(dir, 'import.*.pkl')):
+        df = pd.read_pickle(fn)
+        
+        if format == 'csv':
+            df.to_csv(destFn, mode='a', index=False, header=not os.path.exists(destFn))
+        elif format == 'h5':
+            df.to_hdf(destFn, mode='a', key='catalog', append=True, format='table', complib='blosc:lz4', complevel=9)
+        else:
+            raise Exception(f"Unknown format '{format}'")
         os.unlink(fn)
 
-def csv2hips(hipsPath, urls, k=5):
+def csv2hips(hipsPath, urls, k=5, format='csv'):
     """
     Convert a list of URLs to CSV files (may be local files) to a HiPS file
     of a given order k.
@@ -94,6 +105,7 @@ def csv2hips(hipsPath, urls, k=5):
     from multiprocessing import Pool
 
     summary = None
+    # FIXME: the level of parallelization should be user-settable
     with Pool(12) as pool:
         #
         # Stage #1: Import files in parallel, each into its own leaf .csv file
@@ -115,7 +127,7 @@ def csv2hips(hipsPath, urls, k=5):
         #
         # Stage #2: Merge the individual files into a single catalog.csv file
         #
-        worker = partial(_merge_worker, hipsPath=hipsPath, outFn='catalog.csv')
+        worker = partial(_merge_worker, hipsPath=hipsPath, outFn='catalog', format=format)
         prog = tqdm(pool.imap_unordered(worker, summary.index), total=len(summary))
         for _ in prog:
             pass
@@ -128,9 +140,10 @@ def main():
 #    urls = glob.glob('./gdr2/*.csv.gz')
 
     from .util import get_gaia_csv_urls
-#    urls = get_gaia_csv_urls()[0:1000]
-    urls = get_gaia_csv_urls()[1000:2000]
+    urls = get_gaia_csv_urls()
+    print(f"Number of input files {len(urls)}")
+    urls = urls[:5000]
 
-    summary = csv2hips('output', urls)
+    summary = csv2hips('output', urls, format='h5')
     print(summary)
     print(f'Total rows imported: {summary.sum()}')
