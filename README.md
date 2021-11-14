ea080ff6391cf3481b42e19c0a0e39c11184e5ed
Mario Juric <mjuric@astro.washington.edu>
2021-11-14 00:27:37 -0800

    Gaia importer changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## HiPSCat: Partitioned, query-able, astronomical catalogs
 
+### Quick Test
+
+For a quick test, run:
+```
+python -m hipscat
+```
+which should import a bunch of Gaia GDR2 files into a HiPS file in
+`./output`
--- hipscat/__init__.py
+++ hipscat/__init__.py
@@ -3,63 +3,88 @@
 
 import healpy as hp
 import pandas as pd
-import numpy as np
-import os
-import csv
-import time
-import errno
-from csv import writer
+import os, os.path
+from tqdm import tqdm
 
-def to_hips(long,lat):
+def pix2subdir(k, pix):
+    # return a healpix subdirectory for the given order and pixel number
+    # FIXME: turn this into a proper docstring
+    return f'Norder{k}/Npix{pix}'
+
+def _to_hips(df, hipsPath):
+    # WARNING: only to be used from df2hips(); it's defined out here just for debugging
+    # convenience.
+
+    # grab the order and pix number for this dataframe. Since this function
+    # is intented to be called with apply() after running groupby on (k, pix), these must
+    # be the same throughout the entire dataframe
+    k, pix = df['hips_k'].iloc[0],  df['hips_pix'].iloc[0]
+    assert (df['hips_k']   ==   k).all()
+    assert (df['hips_pix'] == pix).all()
+
+    # construct the output directory and filename
+    dir = os.path.join(hipsPath, pix2subdir(k, pix))
+    fn  = os.path.join(dir, 'catalog.csv')
+
+    # create dir if it doesn't exist
+    os.makedirs(dir, exist_ok=True)
+
+    # write to the file (append if it already exists)
+    # also, write the header only if the file doesn't already exist
+    df.to_csv(fn, mode='a', index=False, header=not os.path.exists(fn))
+
+    # return the number of records written
+    return len(df)
+
+def df2hips(hipsPath, df, k):
+    # Dataframe-to-hips: write out a dataframe as a HiPS file of a given order
+    # FIXME: rewrite this implementation so it doesn't modify the input dataframe
+
+    # FIXME: write a proper docstring
+    df['hips_k'] = k
+    df['hips_pix'] = hp.ang2pix(2**k, df['ra'].values, df['dec'].values, lonlat=True)
+
+    # returns the number of records written to each HiPS file
+    return df.groupby(['hips_k', 'hips_pix']).apply(_to_hips, hipsPath=hipsPath)
+
+def csv2hips(hipsPath, urls, k=5):
     """
-    :param long: ra in degrees
-    :param lat: dec in degrees
-    :return: pix path
+    Convert a list of URLs to CSV files (may be local files) to a HiPS file
+    of a given order k.
+    
+    :param hipsPath: the output HiPS directory
+    :param urls: a list of URLs with GAIA .csv files
+    :param k: Healpix order
+
+    :return: Number of records written to each HiPS file
     """
-    path_name = 'Norder{k}/Npix{pix}/catalog.csv'.format(k=5,pix = hp.ang2pix(theta=long, phi=lat, nside=2**5, lonlat=True))
-    return path_name
+    summary = None
+    prog = tqdm(urls)
+    for url in prog:
+        df = pd.read_csv(url)
+        ret = df2hips(hipsPath, df, k=k)
 
-def csv_to_hips_2(df_csv, k=5):
-    """
+        # Keep track of how many rows we've imported into
+        # each individual HiPS file
+        if summary is None:
+            summary = ret
+        else:
+            # FIXME: this changes the datatype to float... grrr...
+            summary = summary.add(ret, fill_value=0).astype(int)
 
-    :param df_csv: pandas dataframe containing gaia data paths
-    :param k: Nside
-    :return: None
-    """
-    for x in range(0, len(df_csv)):
+        # progress update
+        prog.set_postfix({'rows imported': summary.sum()})
 
-        try:
-            df_csv_file = pd.read_csv(df_csv['Gaia_file_paths'][x])
-            df_csv_file['pix'] = df_csv_file.apply(lambda row: to_hips(row['ra'], row['dec']), axis=1)
-            unique_paths = df_csv_file['pix'].unique().tolist()
-            
-        except FileNotFoundError:
-            print("Gaia File not found.")
+    return summary
 
-        for paths in unique_paths:
+def main():
+    # HACK: for quick tests, run python -m hipscat
+#    import glob
+#    urls = glob.glob('./gdr2/*.csv.gz')
 
-            df_temp = df_csv_file[df_csv_file['pix'] == paths]
-            hips_path = paths.split('cata')[0]
-            print(hips_path)
+    from .util import get_gaia_csv_urls
+    urls = get_gaia_csv_urls()[:10]
 
-            dir_path = '/home/obaiddawarki/Gaia_Data_Analysis/' + hips_path
-
-            if not os.path.exists(dir_path):
-                try:
-                    print("Attempting to create directory {}".format(dir_path))
-                    os.makedirs(dir_path)
-                    print("Succesfully created directory {}".format(dir_path))
-                    abs_path = dir_path + 'catalog.csv'
-                    df_temp.to_csv(abs_path, index=False)
-                except OSError as e:
-                    if e.errno != errno.EEXIST:
-                        raise  # raises the error again
-            else:
-                abs_path = dir_path + 'catalog.csv'
-                df_temp.to_csv(abs_path, mode='a', header=False)
-
-if __name__ == "__main__":
-    file_paths = parse_source_data()
-
-    test_data  = file_paths.head(100)
-    csv_to_hips_2(df_csv=test_data, k=5)
+    summary = csv2hips('output', urls)
+    print(summary)
+    print(f'Total rows imported: {summary.sum()}')
