fa209b2e76e71a220df35ae2d021991b376ec4aa
Mario Juric <mjuric@astro.washington.edu>
2021-12-04 23:46:05 -0800

    pty works!

--- forky.py
+++ forky.py
@@ -1,4 +1,4 @@
-import socket, os, sys, array
+import socket, os, sys, array, struct, pickle, tty, fcntl, termios
 
 # https://gist.github.com/jmhobbs/11276249
 socket_path = os.path.join(os.environ['XDG_RUNTIME_DIR'], 'echo.socket')
@@ -7,10 +7,171 @@ STDIN=0
 STDOUT=1
 STDERR=2
 
+# From http://computer-programming-forum.com/56-python/ef56832eb6f33ba3.htm
+# h, w = struct.unpack("hhhh", fcntl.ioctl(0, termios.TIOCGWINSZ ,"\000"*8))[0:2]
+#
+
+def debug(*argv, **kwargs):
+    kwargs['file'] = sys.stderr
+    return print(*argv, **kwargs)
+
+def _getwinsize(fd):
+    # Get terminal window size for file descriptor fd
+    h, w = struct.unpack("HHHH", fcntl.ioctl(fd, termios.TIOCGWINSZ ,"\000"*8))[0:2]
+    return h, w
+
+def _setwinsize(fd, winsz):
+    # Set window size of tty with file descriptor fd
+    # winsz is a (rows, cols) tuple
+    rows, cols = winsz
+    s = struct.pack('HHHH', rows, cols, 0, 0)
+    res = fcntl.ioctl(fd, termios.TIOCSWINSZ, s)
+
+    r, c = _getwinsize(fd)
+    assert r == rows and c == cols
+
+def _getmask():
+    """Gets signal mask of current thread."""
+    return signal.pthread_sigmask(signal.SIG_BLOCK, [])
+
+def _sigblock():
+    """Blocks all signals."""
+    signal.pthread_sigmask(signal.SIG_BLOCK, ALL_SIGNALS)
+
+def _sigreset(saved_mask):
+    """Restores signal mask."""
+    signal.pthread_sigmask(signal.SIG_SETMASK, saved_mask)
+
+def _winchset(slave_fd, saved_mask, handle_winch):
+    """Installs SIGWINCH handler. Returns old SIGWINCH
+    handler if relevant; returns None otherwise."""
+    bkh = None
+    if handle_winch:
+        def _hwinch(signum, frame):
+            """SIGWINCH handler."""
+            _sigblock()
+            new_slave_fd = os.open(slave_path, os.O_RDWR)
+            tty.setwinsize(new_slave_fd, tty.getwinsize(STDIN_FILENO))
+            os.close(new_slave_fd)
+            _sigreset(saved_mask)
+
+        slave_path = os.ttyname(slave_fd)
+        try:
+            # Raises ValueError if not called from main thread.
+            bkh = signal.signal(SIGWINCH, _hwinch)
+        except ValueError:
+            pass
+
+    return bkh
+
+def openpty(mode=None, winsz=None):
+    """openpty() -> (master_fd, slave_fd)
+    Open a pty master/slave pair, using os.openpty() if possible."""
+
+    master_fd, slave_fd = os.openpty()
+    debug(f"{master_fd=}, {slave_fd=}")
+
+    if mode:
+        termios.tcsetattr(slave_fd, tty.TCSAFLUSH, mode)
+    if winsz:
+        _setwinsize(slave_fd, winsz)
+
+    return master_fd, slave_fd
+
+def _login_tty(fd):
+    """Prepare a terminal for a new login session.
+    Makes the calling process a session leader; the tty of which
+    fd is a file descriptor becomes the controlling tty, the stdin,
+    the stdout, and the stderr of the calling process. Closes fd."""
+    # Establish a new session.
+    os.setsid()
+
+    # The tty becomes the controlling terminal.
+    try:
+        fcntl.ioctl(fd, termios.TIOCSCTTY)
+    except (NameError, OSError):
+        # Fallback method; from Advanced Programming in the UNIX(R)
+        # Environment, Third edition, 2013, Section 9.6 - Controlling Terminal:
+        # "Systems derived from UNIX System V allocate the controlling
+        # terminal for a session when the session leader opens the first
+        # terminal device that is not already associated with a session, as
+        # long as the call to open does not specify the O_NOCTTY flag."
+        tmp_fd = os.open(os.ttyname(fd), os.O_RDWR)
+        os.close(tmp_fd)
+
+    # The tty becomes stdin/stdout/stderr.
+    os.dup2(fd, STDIN)
+    os.dup2(fd, STDOUT)
+#    os.dup2(fd, STDERR)
+
+    if fd != STDIN and fd != STDOUT and fd != STDERR:
+        os.close(fd)
+
+###########################################################################################
+
+def _run_payload(payload):
+    print("Welcome to my echo chamber!")
+    import time, tqdm
+    for _ in tqdm.tqdm(range(100), file=sys.stdout):
+        time.sleep(0.1)
+    for line in sys.stdin:
+        print("ECHO:", line, end='')
+        debug("RECEIVED:", line, end='')
+    debug("Exiting.")
+    exit(0)
+
+    #conn.send(f"Hello from child at {os.getpid()=}\n".encode('utf-8'))
+    # TODO: launch a dameon thread to receive messages over the control socket
+    # This is how we'll receive screen resize mesages, etc., in the future.
+
+    print("Executing payload")
+    exec(payload)
+
+    exit(0)
+
+def _spawn(payload, conn, mode, winsz):
+    #
+    # Spawn a process to execute the Python code. The parent will
+    # stay behind to receive and pass on SIGWINCH and other signals.
+    #
+    master_fd, slave_fd = openpty(mode, winsz)
+    debug(f"Opened PTY {master_fd=} {slave_fd=}")
+
+    pid = os.fork()
+    if pid == 0:
+        # child
+        os.close(master_fd)
+
+	# make us the session leader, and slave_fd our 
+	# controlling terminal and stdin/out/err
+        _login_tty(slave_fd)
+
+        _run_payload(payload)
+
+        exit(0)
+    else:
+        # parent (the control monitor)
+        os.close(slave_fd)
+
+	# shovel messages between our STDIN/STDOUT and the pty
+        _communicate(STDIN, master_fd, master_fd, STDOUT)
+
+#        # continue reading on the control socket, listening for any
+#        # window size change and other signals to deliver.
+#        fd = conn.makefile(encoding='utf-8')
+#        for line in fd:
+#            debug(f"CONTROL MSG: {line}")
+#        debug("_spawn() exiting.")
+
+        # TODO: Should we kill the child process here?
+
+        os.close(master_fd)
+        conn.close()
+
 def _server(preload, payload, timeout=None):
-    print(f"Executing preload")
+    debug(f"Executing preload")
     exec(preload)
-    print("Done.")
+    debug("Done.")
 
     # handle the race condition where two programs are launched
     # at the same time, and try to create the same socket. We'll create
@@ -21,68 +182,59 @@ def _server(preload, payload, timeout=None):
     if os.path.exists(spath):
         os.unlink(spath)
 
-    print(f"Opening socket at {socket_path=} with {timeout=}...", end='')
+    debug(f"Opening socket at {socket_path=} with {timeout=}...", end='')
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
         sock.settimeout(timeout)
         sock.bind(spath)
         sock.listen()
         os.rename(spath, socket_path)
-        print(' done.')
+        debug(' done.')
         while True:
             try:
                 conn, _ = sock.accept()
             except socket.timeout:
-                print("Server timeout. Exiting")
+                debug("Server timeout. Exiting")
                 exit(0)
 
-            print(f"Connection accepted")
+            debug(f"Connection accepted")
             pid = os.fork()
             if pid == 0:
                 # Child process
-                print(f"Forked child at {os.getpid()=}")
+                debug(f"Forked child at {os.getpid()=}")
                 sock.close()
                 # read the command line (see client for protocol description)
                 fd = conn.fileno()
                 hdr = array.array('i')
                 hdr.frombytes(_read_n(fd, 4))
                 msglen = hdr[0]
-                print(f"Message length is {msglen=}")
+                debug(f"Message length is {msglen=}")
                 msg = _read_n(fd, msglen).decode('utf-8')
-                print(f"Raw message is {msg=}")
+                debug(f"Raw message is {msg=}")
                 # parse & reset our sys.argv
                 sys.argv = msg.split('\0')
-                print(f"{sys.argv=}")
+                debug(f"{sys.argv=}")
 
                 # Next are the pipes to communicate with the master
                 msg, (stdin_r, stdout_w), _, _ = socket.recv_fds(conn, 10, maxfds=2)
-                print(f"Received pipes {stdin_r=}, {stdout_w=}")
+                debug(f"Received pipes {stdin_r=}, {stdout_w=}")
 
                 # Dup them as our stdin/stdout/stderr
                 os.dup2(stdin_r, STDIN)
                 os.dup2(stdout_w, STDOUT)
 #                os.dup2(stdout_w, STDERR)
-#                print("Here!")
+#                debug("Here!")
                 os.close(stdin_r)
                 os.close(stdout_w)
-                print("Welcome to my echo chamber!")
-                import time, tqdm
-                for _ in tqdm.tqdm(range(100), file=sys.stdout):
-                    time.sleep(0.1)
-                for line in sys.stdin:
-                    print("ECHO:", line, end='')
-                    print("RECEIVED:", line, end='', file=sys.stderr)
-                print("Exiting.", file=sys.stderr)
+
+                # Next is the window size info and tty attributes
+                debug(f"Receiving window size and tty attributes:")
+                mode, winsz = pickle.loads(_read_msg(fd))
+                debug(f"{winsz=}")
+
+                # Now we fork the process attached to a new pty
+                _spawn(payload, conn, mode, winsz)
                 exit(0)
 
-                #conn.send(f"Hello from child at {os.getpid()=}\n".encode('utf-8'))
-                # TODO: launch a dameon thread to receive messages over the control socket
-                # This is how we'll receive screen resize mesages, etc., in the future.
-                conn.close()
-
-                print("Executing payload")
-                exec(payload)
-
-                exit(0)
             else:
                 # Server
                 conn.close()
@@ -94,10 +246,22 @@ def _read(fd):
 def _read_n(fd, length):
     """Read exactly length bytes"""
     b = os.read(fd, length)
-    print(f"{b=}")
+#    debug(f"{b=}")
     while len(b) != length:
         b += os.read(fd, len(b)-length)
     return b
+
+def _read_msg(fd):
+    msglen, = struct.unpack('I', _read_n(fd, 4))
+#    debug(f"Message length is {msglen=}")
+    msg = _read_n(fd, msglen)
+#    debug(f"Raw message is {msg=}")
+    return msg
+
+def _send_msg(fd, data):
+    # msg format: [length][payload]
+    _write(fd, struct.pack('I', len(data)))
+    _write(fd, data)
 
 def _write(fd, data):
     """Write all the data to a descriptor."""
@@ -105,7 +269,6 @@ def _write(fd, data):
         n = os.write(fd, data)
         data = data[n:]
 
-#def _communicate(master_fd, saved_mask=set(), master_read=_read, stdin_read=_read):
 def _communicate(in_src, in_dest, out_src, out_dest):
     import select
 
@@ -159,9 +322,24 @@ def _connect(preload, payload, timeout=None):
 #    os.close(stdin_w)
 #    os.close(stdout_r)
 
+    # Next is the local tty attributes and window size
+    winsz = _getwinsize(STDOUT)
+    mode = termios.tcgetattr(STDIN)
+    _send_msg(client.fileno(), pickle.dumps((mode, winsz)))
+
+    # switch input to raw mode
+    # See here for _very_ useful info:
+    #   https://stackoverflow.com/questions/51509348/python-tty-setraw-ctrl-c-doesnt-work-getch
+    tty.setraw(STDIN)
+
     # Now enter the communication forwarding loop
 #    os.write(stdin_w, b"Test, writing!\n")
-    _communicate(STDIN, stdin_w, stdout_r, STDOUT)
+    try:
+        _communicate(STDIN, stdin_w, stdout_r, STDOUT)
+    finally:
+        termios.tcsetattr(STDIN, tty.TCSAFLUSH, mode)
+#    if bkh:
+#        signal.signal(SIGWINCH, bkh)
 
 def run(module, func):
     # check if we're already running
